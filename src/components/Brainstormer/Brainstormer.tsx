818ba10a5022cae1ed9addc025b8eb60ef43812e
Josh <jdegraw42@gmail.com>
2018-12-02 20:49:14 -0700

    Brought back lost components

--- src/components/Brainstormer/Brainstormer.tsx
+++ src/components/Brainstormer/Brainstormer.tsx
@@ -7,8 +7,223 @@ import './Brainstormer.scss';
 import Canvas from './Main/Canvas';
 import Graph from './Main/Graph';
 
-export default class Brainstormer extends React.PureComponent {
-    public render() {
+type GraphProps = {
+    onSelected(selected: INode | null): any;
+};
+
+type GraphState = {
+    graph: {
+        nodes: INode[];
+        edges: IEdge[];
+    };
+    selected: INode | IEdge | null;
+    totalNodes: number;
+
+    searchResults: googleFetcher.SearchResults | null;
+    imageResults: googleFetcher.SearchResults | null;
+};
+
+const DEBOUNCE_MS = 50;
+
+class Graph extends React.Component<GraphProps, GraphState> {
+    constructor(props: GraphProps) {
+        super(props);
+
+        this.state = {
+            graph: graphState,
+            selected: graphState.nodes[0],
+            totalNodes: graphState.nodes.length,
+            searchResults: null,
+            imageResults: null,
+        };
+
+        this.handleGoogleSearch = _.debounce(this.handleGoogleSearch.bind(this), DEBOUNCE_MS);
+    }
+
+    public readonly state: GraphState;
+
+    //#region Handlers copied from react-digraph sample graph
+    // Helper to find the index of a given node
+    getNodeIndex(searchNode: INode | any) {
+        return this.state.graph.nodes.findIndex((node: INode) => {
+            return node[NODE_KEY] === searchNode[NODE_KEY];
+        });
+    }
+
+    // Helper to find the index of a given edge
+    getEdgeIndex(searchEdge: IEdge) {
+        return this.state.graph.edges.findIndex((edge: IEdge) => {
+            return edge.source === searchEdge.source && edge.target === searchEdge.target;
+        });
+    }
+    addStartNode = () => {
+        // @ts-ignore
+        const input = (window as any).prompt('Name this node:') || 'New input';
+        this.addNode(input);
+    };
+    addRandomNode = () => {
+        this.addNode('Auto-generated node');
+    };
+
+    addNode = (title: string) => {
+        const graph = this.state.graph;
+        // using a new array like this creates a new memory reference
+        // this will force a re-render
+
+        const newNode: INode = {
+            id: Date.now(),
+            title,
+            type: SPECIAL_TYPE,
+            x: 50,
+            y: 50,
+        };
+
+        graph.nodes = [newNode, ...this.state.graph.nodes];
+        this.setState({
+            graph,
+            selected: newNode,
+        });
+    };
+
+    /*
+     * Handlers/Interaction
+     */
+
+    // Called by 'drag' handler, etc..
+    // to sync updates from D3 with the graph
+    onUpdateNode = (viewNode: INode) => {
+        const graph = this.state.graph;
+        const i = this.getNodeIndex(viewNode);
+
+        graph.nodes[i] = viewNode;
+        this.setState({ graph });
+
+        this.handleGoogleSearch(viewNode);
+    };
+
+    onSelectNode = (viewNode: INode | null) => {
+        this.setState({ selected: viewNode });
+        this.handleGoogleSearch(viewNode);
+    };
+
+    // Edge 'mouseUp' handler
+    onSelectEdge = (viewEdge: IEdge) => {
+        this.setState({ selected: viewEdge });
+    };
+
+    // Updates the graph with a new node
+    onCreateNode = (x: number, y: number) => {
+        const graph = this.state.graph;
+
+        // This is just an example - any sort of logic
+        // could be used here to determine node type
+        // There is also support for subtypes. (see 'sample' above)
+        // The subtype geometry will underlay the 'type' geometry for a node
+        const type = Math.random() < 0.25 ? SPECIAL_TYPE : EMPTY_TYPE;
+
+        const viewNode = {
+            id: Date.now(),
+            title: '',
+            type,
+            x,
+            y,
+        };
+
+        graph.nodes = [...graph.nodes, viewNode];
+        this.setState({ graph });
+        this.handleGoogleSearch(viewNode);
+    };
+
+    // Deletes a node from the graph
+    onDeleteNode = (viewNode: INode, nodeId: string, nodeArr: INode[]) => {
+        const graph = this.state.graph;
+        // Delete any connected edges
+        const newEdges = graph.edges.filter((edge: IEdge, i: number) => {
+            return edge.source !== viewNode[NODE_KEY] && edge.target !== viewNode[NODE_KEY];
+        });
+        graph.nodes = nodeArr;
+        graph.edges = newEdges;
+
+        this.setState({ graph, selected: null });
+    };
+
+    // Creates a new node between two edges
+    onCreateEdge = (sourceViewNode: INode, targetViewNode: INode) => {
+        const graph = this.state.graph;
+        // This is just an example - any sort of logic
+        // could be used here to determine edge type
+        const type = sourceViewNode.type === SPECIAL_TYPE ? SPECIAL_EDGE_TYPE : EMPTY_EDGE_TYPE;
+
+        const viewEdge = {
+            source: sourceViewNode[NODE_KEY],
+            target: targetViewNode[NODE_KEY],
+            type,
+        };
+
+        // Only add the edge when the source node is not the same as the target
+        if (viewEdge.source !== viewEdge.target) {
+            graph.edges = [...graph.edges, viewEdge];
+            this.setState({
+                graph,
+                selected: viewEdge,
+            });
+        }
+    };
+
+    // Called when an edge is reattached to a different target.
+    onSwapEdge = (sourceViewNode: INode, targetViewNode: INode, viewEdge: IEdge) => {
+        const graph = this.state.graph;
+        const i = this.getEdgeIndex(viewEdge);
+        const edge = JSON.parse(JSON.stringify(graph.edges[i]));
+
+        edge.source = sourceViewNode[NODE_KEY];
+        edge.target = targetViewNode[NODE_KEY];
+        graph.edges[i] = edge;
+        // reassign the array reference if you want the graph to re-render a swapped edge
+        graph.edges = [...graph.edges];
+
+        this.setState({
+            graph,
+            selected: edge,
+        });
+    };
+
+    // Called when an edge is deleted
+    onDeleteEdge = (viewEdge: IEdge, edges: IEdge[]) => {
+        const graph = this.state.graph;
+        graph.edges = edges;
+        this.setState({
+            graph,
+            selected: null,
+        });
+    };
+
+    //#endregion
+
+    public componentDidMount() {
+        this.handleGoogleSearch(this.state.graph.nodes[0]);
+    }
+
+    private handleGoogleSearch(viewNode: INode | null) {
+        if (viewNode !== null) {
+            Promise.all([
+                googleFetcher.search(viewNode.title).then(searchResults => {
+                    this.setState({ searchResults });
+                }),
+
+                googleFetcher.imageSearch(viewNode.title).then(imageResults => {
+                    this.setState({ imageResults });
+                }),
+            ]);
+        } else {
+            this.setState({ imageResults: null, searchResults: null });
+        }
+    }
+    render() {
+        const { nodes, edges } = this.state.graph;
+        const selected = this.state.selected;
+        const { NodeTypes, NodeSubtypes, EdgeTypes } = GraphConfig;
+
         return (
             <>
                 <Graph />
