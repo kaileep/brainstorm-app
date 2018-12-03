a0fdbd8ad035bcf6c441cda244b0fb1fa81c3d1f
Josh <jdegraw42@gmail.com>
2018-12-02 21:01:55 -0700

    Updated sidebar style

--- src/components/Brainstormer/Sidebar/Sidebar.tsx
+++ src/components/Brainstormer/Sidebar/Sidebar.tsx
@@ -2,34 +2,33 @@ import * as React from 'react';
 import SidebarButton from './Buttons/SidebarButton';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import './Sidebar.scss';
-import {FormGroup, Label, Input, FormText, Form} from 'reactstrap';
+import { FormGroup, Label, Input, FormText, Form } from 'reactstrap';
 
 export default function Sidebar(props: SidebarProps) {
     return (
         <section className="side-bar">
             <ListGroup>
-                <SidebarButton onIconClick={props.onCustomIdeaClick}>
-                    Add Custom idea
-                </SidebarButton>
-                <SidebarButton onIconClick={props.onAutoClick}>
-                    Add Auto-generated idea
-                </SidebarButton>
+                <SidebarButton onIconClick={props.onCustomIdeaClick}>Add Custom idea</SidebarButton>
+                <SidebarButton onIconClick={props.onAutoClick}>Add Auto-generated idea</SidebarButton>
             </ListGroup>
-            <Form>
-            <FormGroup>
-          <Label for="exampleFile" alignment="center">Upload Project File</Label>
-          <Input type="file" name="file" id="exampleFile" />
-          <FormText color="muted">
-          Already got a brainstorm going? Upload it here to continue working!
-          </FormText>
-            </FormGroup>
-            </Form>
-
+            <ListGroupItem>
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleFile" alignment="center">
+                            Upload Project File
+                        </Label>
+                        <Input type="file" name="file" id="exampleFile" />
+                        <FormText color="muted">
+                            Already got a brainstorm going? Upload it here to continue working!
+                        </FormText>
+                    </FormGroup>
+                </Form>
+            </ListGroupItem>
         </section>
     );
 }
 
 export interface SidebarProps {
-    onCustomIdeaClick():void;
-    onAutoClick():void
+    onCustomIdeaClick(): void;
+    onAutoClick(): void;
 }
